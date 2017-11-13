13ebf903058070d141566fdab4e7f1ccc0d914a4
wsp13 <willanepaiva@hotmail.com>
2017-11-13 11:48:11 -0300

    Remoção das coisas do doxygen

--- QtTcpClientConsumer/html/search/functions_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['paintevent',['paintEvent',['../class_plotter.html#a06477bf987646f000a8982db1352a11d',1,'Plotter']]],
-  ['plotter',['Plotter',['../class_plotter.html#a9d8f6981cf177bc3e843465d2b3425e9',1,'Plotter']]]
-];
