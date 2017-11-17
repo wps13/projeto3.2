89e1de2c74c4d9836c6568adde5900eb4d219ee3
Willane Paiva <willanepaiva@hotmail.com>
2017-11-17 08:39:43 -0300

    Add files via upload

---
+++ plotter.cpp
@@ -0,0 +1,107 @@
+#include "plotter.h"
+#include <QPainter>
+#include <QBrush>
+#include <QPen>
+#include <cmath>
+
+
+using namespace std;
+/**
+ * @brief Plotter::Plotter Construtor da Classe
+ * @param parent
+ */
+Plotter::Plotter(QWidget *parent) : QWidget(parent)
+{
+
+}
+
+/**
+ * @brief Plotter::paintEvent Realiza o desenho da tela
+ * @param event
+ */
+void Plotter::paintEvent(QPaintEvent *event)
+{
+    /**
+     * @brief painter
+     */
+    QPainter painter(this);
+
+    /**
+     * @brief brush
+     */
+    QBrush brush;
+
+    /**
+     * @brief pen
+     */
+    QPen pen;
+
+
+
+    //define a cor e tipo do preenchimento do fundo da tela
+    //brush.setColor(QColor(255,255,100));
+    brush.setColor(QColor(51,51,51));
+
+    brush.setStyle(Qt::SolidPattern);
+
+    //define a cor da borda da tela
+    //pen.setColor(QColor(255,0,0));
+    pen.setColor(QColor(153,153,153));
+
+    //define a largura da borda
+    pen.setWidth(2);
+
+
+    painter.setBrush(brush);
+
+    painter.setPen(pen);
+
+
+    painter.drawRect(0,0,width(),height());
+    /*
+    pen.setColor(QColor(255,180,0));
+    painter.setPen(pen);
+    */
+
+    //define a cor das linhas da tela
+
+    //pen.setColor(QColor(0,0,255));
+    pen.setColor(QColor(255,255,255));
+    painter.setPen(pen);
+
+
+}
+
+void Plotter::plotGrafico(vector<qint64> &t, vector<int> &v)
+{
+    int x = t.max_size();
+    int y = v.max_size();
+    vector<long> t1;
+    vector<int> v1;
+
+    for(int i;i<t.size();i++)
+        t1[i]=t[i]/x;
+    for(int j;j<v.size();j++)
+        v1[j]=v[j]/y;
+
+     int x1,x2,y1,y2;
+
+     //ALTERAR AS VARIAVÉIS DE FORMA QUE O GRÁFICO SEJA DO TEMPO X VALOR DO SERVIDOR
+
+     //define as posições iniciais
+     x1=t1[0];
+     y1=v1[0];
+
+     //desenha várias retas na tela de forma a desenhar o gráfico
+     for(int i=1;i<t1.size();i++)
+     {
+         x2=t1[i];
+         y2=v[i]; //valor a ser alterado
+         x1=x2;
+         y1=y2;
+
+           repaint();
+     }
+
+}
+
